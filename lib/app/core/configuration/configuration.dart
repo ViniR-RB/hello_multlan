08b309a492e8d0ffe891ef4d75f3c71f9d44d9a0
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2024-07-15 10:41:25 -0300

    feat:validation env

--- lib/app/core/configuration/configuration.dart
+++ lib/app/core/configuration/configuration.dart
@@ -1,4 +1,10 @@
 sealed class Configuration {
   static const String _baseUrl = String.fromEnvironment("API_URL");
   static String get baseUrl => _baseUrl;
+
+  static void validate() {
+    if (_baseUrl.isEmpty) {
+      throw AssertionError('A variável de ambiente API_URL não está definida');
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:hellomultlan/app/app_widget.dart';
+import 'package:hellomultlan/app/core/configuration/configuration.dart';
+import 'package:hellomultlan/app/pages/error_page.dart';
 
 void main() {
+  Configuration.validate();
   runZonedGuarded(() => runApp(const AppWidget()), (error, stack) {
     log("Erro Não Tratado", error: error, stackTrace: stack);
-    throw error;
+    runApp(ErrorPage(message: error.toString()));
   });
 }
